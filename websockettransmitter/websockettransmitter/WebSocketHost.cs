d6f91bbcba11d37663274a16ddd2288402a64b9e
Dag <dagspot@fastmail.fm>
2013-06-13 00:22:03 +0200

    Implements the websockettransmitter
    This class implements listening to incoming websocket connections
    and setting up an outgoing websocket connection to the cloud.
    
    It has a Send method that takes a string, the string will be sent
    to the connected clients, as well as to the cloud.

---
+++ websockettransmitter/websockettransmitter/WebSocketHost.cs
@@ -0,0 +1,107 @@
+﻿using SuperSocket.SocketBase;
+using SuperWebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocket4Net;
+
+namespace websockettransmitter
+{
+    public class WebSocketHost : IDisposable
+    {
+        private WebSocketServer appServer;
+        private static WebSocket client;
+        private static bool clientOpen = false;
+        private static List<WebSocketSession> sessions = new List<WebSocketSession>();
+
+
+        public WebSocketHost()
+        {
+        }
+
+        public bool Setup()
+        {
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+            appServer = new WebSocketServer();
+
+            //Setup the appServer
+            if (!appServer.Setup(2012)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return false;
+            }
+
+            appServer.NewSessionConnected += new SessionHandler<WebSocketSession>(appServer_NewSessionConnected);
+            appServer.SessionClosed += new SessionHandler<WebSocketSession, CloseReason>(appServer_SessionClosed);
+            appServer.NewMessageReceived += new SessionHandler<WebSocketSession, string>(appServer_NewMessageReceived);
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                return false;
+            }
+
+            // Connect to the cloud
+            client = new WebSocket("ws://garagepurple.cloudapp.net:2012");
+            client.Opened += new EventHandler(webSocketClient_Opened);
+            client.Closed += new EventHandler(webSocketClient_Closed);
+            client.MessageReceived += new EventHandler<MessageReceivedEventArgs>(webSocketClient_MessageReceived);
+            client.Open();
+
+            return true;
+        }
+
+        public void Send(string message)
+        {
+            if (clientOpen == true)
+                client.Send(message);
+            foreach (var s in sessions)
+            {
+                s.Send(message);
+            }
+        }
+
+        public bool IsCloudConnected()
+        {
+            return clientOpen;
+        }
+
+        private void webSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            // Ignore messages from the cloud
+        }
+
+        private void webSocketClient_Closed(object sender, EventArgs e)
+        {
+            clientOpen = false;
+        }
+
+        private void webSocketClient_Opened(object sender, EventArgs e)
+        {
+            clientOpen = true;
+        }
+        public void Dispose()
+        {
+            appServer.Dispose();
+        }
+        private static void appServer_SessionClosed(WebSocketSession session, CloseReason value)
+        {
+            sessions.Remove(session);
+        }
+
+        private static void appServer_NewSessionConnected(WebSocketSession session)
+        {
+            sessions.Add(session);
+        }
+
+        static void appServer_NewMessageReceived(WebSocketSession session, string message)
+        {
+            // Ignore messages from the web clients
+        }
+    }
+}
---
+++ websockettransmitter/websockettransmittertest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace websockettransmittertest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StartServer()
+        {
+            websockettransmitter.WebSocketHost host = new websockettransmitter.WebSocketHost();
+            Assert.IsTrue(host.Setup());
+            Thread.Sleep(5000);
+            Assert.IsTrue(host.IsCloudConnected());
+            // Send some test data.
+            // The data will be send to the cloud
+            // as well as to the locally connected websocket clients
+            host.Send("Send");
+            Thread.Sleep(5000);
+            host.Send("some");
+            Thread.Sleep(5000);
+            host.Send("test");
+            Thread.Sleep(5000);
+            host.Send("data");
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
